2f233c2e765af666cc1d17d74f54d3e0facc3493
Jayk <jay.khavadia@rlogical.com>
2025-01-30 16:32:18 +0530

    - used bootstrap component

--- utils/orders/src/lib/orders.jsx
+++ utils/orders/src/lib/orders.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import Button from 'react-bootstrap/Button';
 
 export const Orders = () => {
   return (
-    <div>
-      <h2>Orders Component</h2>
-      <p>This is the shared Orders component from the utils library.</p>
-
+    <div className="p-4">
+      <h2>Orders Page</h2>
+      <Button variant="primary">Place Order</Button>
       {/* Bootstrap Button */}
       <button className="btn btn-primary">Bootstrap Button</button>
     </div>
